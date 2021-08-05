7cc659cfd40e22eff7b77efb8d076793ba04d440
qz-cqy <qz-cqy@outlook.com>
2021-08-05 22:12:31 +0800

    更新

--- script/codeforces.js
+++ script/codeforces.js
@@ -12,6 +12,20 @@ function getCFUser(level, rating) { // user rating
     if (level == 'LGM') return `<p class='rated legendary-grandmaster'>Legendary Grandmaster, ${rating}</p>`;
     return `<p class='rated headquarters'>Headquarters, ${rating}</p>`;
 }
+function getCFUser2(level, name) { // user rating
+    if (level == 'U') return `<p class='unrated'>${name}</p>`;
+    if (level == 'N') return `<p class='rated newbie'>${name}</p>`;
+    if (level == 'P') return `<p class='rated pupil'>${name}</p>`;
+    if (level == 'S') return `<p class='rated specialist'>${name}</p>`;
+    if (level == 'E') return `<p class='rated expert'>${name}</p>`;
+    if (level == 'CM') return `<p class='rated candidate-master'>${name}</p>`;
+    if (level == 'M') return `<p class='rated master'>${name}</p>`;
+    if (level == 'IM') return `<p class='rated international-master'>${name}</p>`;
+    if (level == 'GM') return `<p class='rated grandmaster'>${name}</p>`;
+    if (level == 'IGM') return `<p class='rated international-grandmaster'>${name}</p>`;
+    if (level == 'LGM') return `<p class='rated legendary-grandmaster'>${name}</p>`;
+    return `<p class='rated headquarters'>${name}</p>`;
+}
 function getCFDelta(delta) { // delta / contribution
     if(delta == '0') return `<p class='zero'>${delta}</p>`;
     if(delta[0] == '-') return `<p class='negative'>${delta}</p>`;
