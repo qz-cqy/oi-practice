8bbbb551fac7ee9d5f7baabcff02cfc86b742866
qz-cqy <qz-cqy@outlook.com>
2021-08-06 12:57:18 +0800

    fix

--- script/codeforces.js
+++ script/codeforces.js
@@ -14,19 +14,19 @@ function getCFUser(level, rating) { // user rating
     return `<p class='rated headquarters'>Headquarters, ${rating}</p>`;
 }
 function getCFUserShortLevel(level, rating) { // user rating
-    if (level == null) return `<p>N/A</p>`;
-    if (level == 'U') return `<p class='unrated'>U${rating}</p>`;
-    if (level == 'N') return `<p class='rated newbie'>N${rating}</p>`;
-    if (level == 'P') return `<p class='rated pupil'>P${rating}</p>`;
-    if (level == 'S') return `<p class='rated specialist'>S${rating}</p>`;
-    if (level == 'E') return `<p class='rated expert'>E${rating}</p>`;
-    if (level == 'CM') return `<p class='rated candidate-master'>CM${rating}</p>`;
-    if (level == 'M') return `<p class='rated master'>M${rating}</p>`;
-    if (level == 'IM') return `<p class='rated international-master'>IM${rating}</p>`;
-    if (level == 'GM') return `<p class='rated grandmaster'>Grandmaster, GM${rating}</p>`;
-    if (level == 'IGM') return `<p class='rated international-grandmaster'>IGM${rating}</p>`;
-    if (level == 'LGM') return `<p class='rated legendary-grandmaster'>LGM${rating}</p>`;
-    return `<p class='rated headquarters'>HQ${rating}</p>`;
+    if (level == null) return `<span>N/A</span>`;
+    if (level == 'U') return `<span class='unrated'>U${rating}</span>`;
+    if (level == 'N') return `<span class='rated newbie'>N${rating}</span>`;
+    if (level == 'P') return `<span class='rated pupil'>P${rating}</span>`;
+    if (level == 'S') return `<span class='rated specialist'>S${rating}</span>`;
+    if (level == 'E') return `<span class='rated expert'>E${rating}</span>`;
+    if (level == 'CM') return `<span class='rated candidate-master'>CM${rating}</span>`;
+    if (level == 'M') return `<span class='rated master'>M${rating}</span>`;
+    if (level == 'IM') return `<span class='rated international-master'>IM${rating}</span>`;
+    if (level == 'GM') return `<span class='rated grandmaster'>Grandmaster, GM${rating}</span>`;
+    if (level == 'IGM') return `<span class='rated international-grandmaster'>IGM${rating}</span>`;
+    if (level == 'LGM') return `<span class='rated legendary-grandmaster'>LGM${rating}</span>`;
+    return `<span class='rated headquarters'>HQ${rating}</span>`;
 }
 function getCFUser2(level, name) { // user rating
     if (level == null) return `<p>N/A</p>`;
