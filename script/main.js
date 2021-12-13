b5ed8f93e27a45318ab740908413a1b5e4590ded
qz-cqy <qz-cqy@outlook.com>
2021-12-13 19:23:43 +0800

    /qidao

--- script/main.js
+++ script/main.js
@@ -12,3 +12,15 @@ function qpow(x, y) {
     for(;y;y>>=1,x*=x) if(y&1) ans *= x;
     return ans;
 }
+function setClass(name, classname) {
+    $(name).addClass(classname);
+}
+(function () {
+    var sel = $("html");
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    if(month == 12 && day == 13) {
+        setClass("html", "gray");
+    }
+})();
