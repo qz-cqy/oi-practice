c89ecf478dc96c0856305ddcda9746e50c19a597
qz-cqy <qz-cqy@outlook.com>
2022-02-08 18:44:15 +0800

    改挂了

--- script/atcoder.js
+++ script/atcoder.js
@@ -1,99 +1,99 @@
-function getATUser(level, rating) {
+function getATUser(level, rating, beta) {
     if (level == null) return `${showNA()}`;
     if(level == 'U') { // unrated
-        return `<p class='user at-unrated'>Unrated</p>`;
+        return `<p class='user at-unrated'>Unrated${beta?"β":""}</p>`;
     }
     if(level == 'K') { // king
-        return `<p class='user red'>King, ${rating}</p>`;
+        return `<p class='user red'>King, ${rating}${beta?"β":""}</p>`;
     }
     if(level == "L") { // legend
-        return `<p class='user red'>Legend, ${rating}</p>`;
+        return `<p class='user red'>Legend, ${rating}${beta?"β":""}</p>`;
     }
     var lv_arr = level.match(/KYU/ig);
     if(lv_arr != null) { // is KYU
         var num_arr = level.match(/\d+/g);
         var num = num_arr[0];
-        if(num >= 9) return `<p class='user gray'>${level}, ${rating}</p>`; // 9~20
-        if(num >= 7) return `<p class='user brown'>${level}, ${rating}</p>`;
-        if(num >= 5) return `<p class='user green'>${level}, ${rating}</p>`;
-        if(num >= 3) return `<p class='user cyan'>${level}, ${rating}</p>`;
-        return `<p class='user blue'>${level}, ${rating}</p>`;
+        if(num >= 9) return `<p class='user gray'>${level}, ${rating}${beta?"β":""}</p>`; // 9~20
+        if(num >= 7) return `<p class='user brown'>${level}, ${rating}${beta?"β":""}</p>`;
+        if(num >= 5) return `<p class='user green'>${level}, ${rating}${beta?"β":""}</p>`;
+        if(num >= 3) return `<p class='user cyan'>${level}, ${rating}${beta?"β":""}</p>`;
+        return `<p class='user blue'>${level}, ${rating}${beta?"β":""}</p>`;
     }
     else { // is DAN
         var num_arr = level.match(/\d+/g);
         var num = num_arr[0];
-        if(num <= 2) return `<p class='user yellow'>${level}, ${rating}</p>`;
-        if(num <= 4) return `<p class='user orange'>${level}, ${rating}</p>`;
-        return `<p class='user red'>${level}, ${rating}</p>`;
+        if(num <= 2) return `<p class='user yellow'>${level}, ${rating}${beta?"β":""}</p>`;
+        if(num <= 4) return `<p class='user orange'>${level}, ${rating}${beta?"β":""}</p>`;
+        return `<p class='user red'>${level}, ${rating}${beta?"β":""}</p>`;
     }
 }
-function getATUserShortLevel(level, rating) {
+function getATUserShortLevel(level, rating, beta) {
     if (level == null) return `${showNA_span()}`;
     if(level == 'U') { // unrated
-        return `<span class='user at-unrated'>U</span>`;
+        return `<span class='user at-unrated'>U${beta?"β":""}</span>`;
     }
     if(level == 'K') { // king
-        return `<span class='user red'>K${rating}</span>`;
+        return `<span class='user red'>K${rating}${beta?"β":""}</span>`;
     }
     if(level == "L") { // legend
-        return `<p class='user red'>L${rating}</p>`;
+        return `<p class='user red'>L${rating}${beta?"β":""}</p>`;
     }
     var lv_arr = level.match(/KYU/ig);
     if(lv_arr != null) { // is KYU
         var num_arr = level.match(/\d+/g);
         var num = num_arr[0];
-        if(num >= 9) return `<span class='user gray'>${num}K${rating}</span>`; // 9~20
-        if(num >= 7) return `<span class='user brown'>${num}K${rating}</span>`;
-        if(num >= 5) return `<span class='user green'>${num}K${rating}</span>`;
-        if(num >= 3) return `<span class='user cyan'>${num}K${rating}</span>`;
-        return `<span class='user blue'>${num}K${rating}</span>`;
+        if(num >= 9) return `<span class='user gray'>${num}K${rating}${beta?"β":""}</span>`; // 9~20
+        if(num >= 7) return `<span class='user brown'>${num}K${rating}${beta?"β":""}</span>`;
+        if(num >= 5) return `<span class='user green'>${num}K${rating}${beta?"β":""}</span>`;
+        if(num >= 3) return `<span class='user cyan'>${num}K${rating}${beta?"β":""}</span>`;
+        return `<span class='user blue'>${num}K${rating}${beta?"β":""}</span>`;
     }
     else { // is DAN
         var num_arr = level.match(/\d+/g);
         var num = num_arr[0];
-        if(num <= 2) return `<span class='user yellow'>${num}D${rating}</span>`;
-        if(num <= 4) return `<span class='user orange'>${num}D${rating}</span>`;
-        return `<span class='user red'>${num}D${rating}</span>`;
+        if(num <= 2) return `<span class='user yellow'>${num}D${rating}${beta?"β":""}</span>`;
+        if(num <= 4) return `<span class='user orange'>${num}D${rating}${beta?"β":""}</span>`;
+        return `<span class='user red'>${num}D${rating}${beta?"β":""}</span>`;
     }
 }
-function getATUser2(level, name) {
-    if(level == null) return `<p class='user at-unrated'>${name}</p>`;
+function getATUser2(level, name, beta) {
+    if(level == null) return `<p class='user at-unrated'>${name}${beta?"β":""}</p>`;
     if(level == 'U') { // unrated
-        return `<p class='user at-unrated'>${name}</p>`;
+        return `<p class='user at-unrated'>${name}${beta?"β":""}</p>`;
     }
     if(level == 'K') { // king
-        return `<p class='user red'>${name}</p>`;
+        return `<p class='user red'>${name}${beta?"β":""}</p>`;
     }
     if(level == "L") { // legend
-        return `<p class='user red'>${name}</p>`;
+        return `<p class='user red'>${name}${beta?"β":""}</p>`;
     }
     var lv_arr = level.match(/KYU/ig);
     if(lv_arr != null) { // is KYU
         var num_arr = level.match(/\d+/g);
         var num = num_arr[0];
-        if(num >= 9) return `<p class='user gray'>${name}</p>`; // 9~20
-        if(num >= 7) return `<p class='user brown'>${name}</p>`;
-        if(num >= 5) return `<p class='user green'>${name}</p>`;
-        if(num >= 3) return `<p class='user cyan'>${name}</p>`;
-        return `<p class='user blue'>${name}</p>`;
+        if(num >= 9) return `<p class='user gray'>${name}${beta?"β":""}</p>`; // 9~20
+        if(num >= 7) return `<p class='user brown'>${name}${beta?"β":""}</p>`;
+        if(num >= 5) return `<p class='user green'>${name}${beta?"β":""}</p>`;
+        if(num >= 3) return `<p class='user cyan'>${name}${beta?"β":""}</p>`;
+        return `<p class='user blue'>${name}${beta?"β":""}</p>`;
     }
     else { // is DAN
         var num_arr = level.match(/\d+/g);
         var num = num_arr[0];
-        if(num <= 2) return `<p class='user yellow'>${name}</p>`;
-        if(num <= 4) return `<p class='user orange'>${name}</p>`;
-        return `<p class='user red'>${name}</p>`;
+        if(num <= 2) return `<p class='user yellow'>${name}${beta?"β":""}</p>`;
+        if(num <= 4) return `<p class='user orange'>${name}${beta?"β":""}</p>`;
+        return `<p class='user red'>${name}${beta?"β":""}</p>`;
     }
 }
 function getATProvisional(provisional) {
     if(provisional == true) return `<p class='active'>+</p>`;
     else return `<p class='inactive'>-</p>`;
 }
-function getATDelta(delta) {
+function getATDelta(delta, beta) {
     if (delta == null) return `${showNA()}`;
-    if(delta == '0') return `<p class='inactive'>${delta}</p>`;
-    if(delta[0] == '-') return `<p class='inactive'>${delta}</p>`;
-    return `<p class='active'>${delta}</p>`;
+    if(delta == '0') return `<p class='inactive'>${delta}${beta?"(β)":""}</p>`;
+    if(delta[0] == '-') return `<p class='inactive'>${delta}${beta?"(β)":""}</p>`;
+    return `<p class='active'>${delta}${beta?"(β)":""}</p>`;
 }
 function getATLevel(rated, rating) {
     if (rated == false) return null;
